9f45b9c92f53054194bfd36891ff840831b1ce6b
iCielin <sky@sirlovespikachu.uk>
2020-07-19 14:27:44 +0100

    Small changes

--- src/main/java/uk/kaidev/randomos/CommandFakePrefix.java
+++ src/main/java/uk/kaidev/randomos/CommandFakePrefix.java
@@ -11,26 +11,39 @@ public class CommandFakePrefix implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("fakeprefix")) {
 			String prefix = "";
-			try {
-				if (args[0].equalsIgnoreCase("owner")) { prefix += "&6&lOwner &r"; } 
-				else if (args[0].equalsIgnoreCase("operator")) { prefix += "&b&lOperator &r"; } 
-				else if (args[0].equalsIgnoreCase("admin")) { prefix += "&c&lAdmin &r"; }
-				else if (args[0].equalsIgnoreCase("moderator")) { prefix += "&9&lModerator &r"; }
-				else if (args[0].equalsIgnoreCase("watcher")) { prefix += "&e&lWatcher &r"; }
-				else if (args[0].equalsIgnoreCase("god")) { prefix += "&dGod &r"; }
-				else if (args[0].equalsIgnoreCase("plus")) { prefix += "&aPlus &r"; }
-				else if (args[0].equalsIgnoreCase("default")) { prefix += "&7Player &r"; }
-				else { throw new NullPointerException(); }
-				
-				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), MessageFormat.format(Globals.formatRemovePrefix, args[1], Globals.priorityCodeFPX));
-				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), MessageFormat.format(Globals.formatSetPrefix, args[1], Globals.priorityCodeFPX, prefix));
-				sender.sendMessage(MessageFormat.format("§bUser §f{0}§b now has their prefix set to {1}. §7§oCODE: {2}.", args[1], args[0], Globals.priorityCodeFPX));
-				return true;
-			}
-			catch (NullPointerException e) {
+			switch (args[0]) {
+			case "owner":
+				prefix += "&6&lOwner &r";
+				break;
+			case "operator":
+				prefix += "&b&lOperator &r"; 
+				break;
+			case "admin":
+				prefix += "&c&lAdmin &r";
+				break;
+			case "moderator":
+				prefix += "&9&lModerator &r";
+				break;
+			case "watcher":
+				prefix += "&e&lWatcher &r";
+				break;
+			case "god":
+				prefix += "&dGod &r";
+				break;
+			case "plus":
+				prefix += "&aPlus &r";
+				break;
+			case "default":
+				prefix += "&7Default &r";
+				break;
+			default:
 				sender.sendMessage("§cThat rank isn't correct.");
 				return true;
 			}
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), MessageFormat.format(Globals.formatRemovePrefix, args[1], Globals.priorityCodeFPX));
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), MessageFormat.format(Globals.formatSetPrefix, args[1], Globals.priorityCodeFPX, prefix));
+			sender.sendMessage(MessageFormat.format("§bUser §f{0}§b now has their prefix set to {1}. §7§oCODE: {2}.", args[1], args[0], Globals.priorityCodeFPX));
+			return true;
 		}
 		return false;
 	}
